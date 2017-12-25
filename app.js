4234e5e3d748cfe614fa6aad2f7a47c22dcfdd48
ctilkxxx <kirsanau@gmail.com>
2017-12-25 10:52:43 +0400

    custom luis app

--- app.js
+++ app.js
@@ -41,19 +41,24 @@ var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
 var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+// const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/274d51cc-2037-44a8-91ee-87a829b1b2c6?subscription-key=91798b1a1cde4776bcc9a55893f1e4da&verbose=true&timezoneOffset=0&q='
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+.matches('MyAllowances', (session) => {
+    session.send('You reached MyAllowances intent, you said \'%s\'.', session.message.text);
 })
-.matches('Help', (session) => {
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+.matches('AllowanceSpecific', (session) => {
+    session.send('You reached AllowanceSpecific intent, you said \'%s\'.', session.message.text);
 })
-.matches('Cancel', (session) => {
-    session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+.matches('AllowanceList', (session) => {
+    session.send('You reached AllowanceList intent, you said \'%s\'.', session.message.text);
+})
+.matches('AllowanceDetails', (session) => {
+    session.send('You reached AllowanceDetails intent, you said \'%s\'.', session.message.text);
 })
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
