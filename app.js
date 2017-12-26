5a549ed3edd6e57198e1f9110a46f5c98d23385d
ctilkxxx <kirsanau@gmail.com>
2017-12-26 10:10:02 +0400

    updates with 2 dialogues

--- app.js
+++ app.js
@@ -22,19 +22,24 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
+// // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// var bot = new builder.UniversalBot(connector, function (session) {
+//     session.send("You said: %s", session.message.text);
+// });
+
 /*----------------------------------------------------------------------------------------
 * Bot Storage: This is a great spot to register the private state storage for your bot. 
 * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
 
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+// var tableName = 'botdata';
+// var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+// var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector);
-bot.set('storage', tableStorage);
+// bot.set('storage', tableStorage);
 
 // Make sure you add code to validate these fields
 var luisAppId = process.env.LuisAppId;
@@ -47,25 +52,90 @@ const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('MyAllowances', (session) => {
-    session.send('You reached MyAllowances intent, you said \'%s\'.', session);
-})
-.matches('AllowanceSpecific', (session) => {
-    session.send('You reached AllowanceSpecific intent, you said \'%s\'.', session.message.text);
-})
-.matches('AllowanceList', (session) => {
-    session.send('You reached AllowanceList intent, you said \'%s\'.', session.message.text);
-})
-.matches('AllowanceDetails', (session) => {
-    session.send('You reached AllowanceDetails intent, you said \'%s\'.', session.message.text);
-})
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+bot.recognizer(recognizer);
+// var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+// .matches('MyAllowances', (session) => {
+//     session.send('You reached MyAllowances intent, you said \'%s\'.', session);
+// })
+// .matches('AllowanceSpecific', (session) => {
+//     session.send('You reached AllowanceSpecific intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('AllowanceList', (session) => {
+//     session.send('You reached AllowanceList intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('AllowanceDetails', (session) => {
+//     session.send('You reached AllowanceDetails intent, you said \'%s\'.', session.message.text);
+// })
+// /*
+// .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+// */
+// .onDefault((session) => {
+//     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+// });
+
+// bot.dialog('/', intents);    
+
+bot.dialog('AllowanceListDialog',
+    (session, args) => {
+        // Resolve and store any HomeAutomation.Device entity passed from LUIS.
+        var intent = args.intent;
+        session.send('You reached AllowanceListDialog intent, you said \'%s\'.', session.message.text);
+
+        var msg = new builder.Message(session).addAttachment(createPolicyListCard(session));
+        session.send(msg);
+
+        session.send('Please select one of the allowances to get more details');
+
+        session.endDialog();
+    }
+).triggerAction({
+    matches: 'AllowanceList'
 });
 
-bot.dialog('/', intents);    
+
+function createPolicyListCard(session) {
+    return new builder.HeroCard(session)
+        .title('Adnoc allowances')
+        .subtitle('Topics')
+        .text('Please select one you are interested in, it will take to intranet portal relevant page.')
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://google.com', 'General Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Social Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Child Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Furniture Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Remote Area Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Shift Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Transportation Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Telephone Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Duty Travel Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Air travel Allowance'),
+            builder.CardAction.openUrl(session, 'https://google.com', 'Housing Allowance')
+        ]);
+}
+
+bot.dialog('AllowanceSpecificDialog',
+    (session, args) => {
+        // Resolve and store any HomeAutomation.Device entity passed from LUIS.
+        var intent = args.intent;
+        session.send('You reached AllowanceSpecificDialog intent, you said \'%s\'.', session.message.text);
+        var nationality = builder.EntityRecognizer.findEntity(intent.entities, 'Nationality');
+        var grade = builder.EntityRecognizer.findEntity(intent.entities, 'Grade');
+        var allowanceType = builder.EntityRecognizer.findEntity(intent.entities, 'AllowanceType');
+
+        // Turn on a specific device if a device entity is detected by LUIS
+        if (allowanceType) {
+            session.send('Ok, you are interested in allowance: %s', allowanceType.entity);
+            session.send(' for nationality: %s', nationality.entity);
+            session.send( ' and grade: %s', grade.entity);
+            // Put your code here for calling the IoT web service that turns on a device
+        } else {
+            // Assuming turning on lights is the default
+            session.send('Can you please specify which of allowances are you interested in');
+            // Put your code here for calling the IoT web service that turns on a device
+        }
+        session.endDialog();
+    }
+).triggerAction({
+    matches: 'AllowanceSpecific'
+});
 
